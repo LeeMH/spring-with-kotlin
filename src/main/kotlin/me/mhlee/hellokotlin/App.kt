23c15bf18368ab06da3517c922434a03963ad04b
mhlee <lee.munhaeng@gmail.com>
2021-10-26 00:18:50 +0900

    프로젝트 생성

---
+++ src/main/kotlin/me/mhlee/hellokotlin/App.kt
@@ -0,0 +1,11 @@
+package me.mhlee.hellokotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App
+
+fun main(args: Array<String>) {
+	runApplication<App>(*args)
+}
